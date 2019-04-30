2030997000b3166bbb422737a2e166948f50bf6c
Manas <prometheansacrifice@gmail.com>
2019-04-30 20:52:08 +0530

    Escape compilers in the --args

--- esy/build.sh
+++ esy/build.sh
@@ -14,7 +14,7 @@ if [[ $OS == 'windows' ]]
 then
     bin/gn gen $cur__target_dir/out/Release --args='is_debug=false' || exit -1
 else
-    bin/gn gen $cur__target_dir/out/Release "--args=cc=clang cxx=clang++ skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
+    bin/gn gen $cur__target_dir/out/Release "--args=cc=\"clang\" cxx=\"clang++\" skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
 fi
 
 ninja.exe -C $cur__target_dir/out/Release
